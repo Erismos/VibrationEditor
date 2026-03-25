e458c032b820a4315b0ed4b32810f15b961d3385
Clément Auvray <114954624+Erismos@users.noreply.github.com>
2026-03-24 23:09:02 -0400

    Working basic nav

--- app/src/main/java/com/example/vibrationeditor/MainActivity.kt
+++ app/src/main/java/com/example/vibrationeditor/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
@@ -12,14 +14,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.vibrationeditor.ui.theme.VibrationEditorTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,30 +42,49 @@ class MainActivity : ComponentActivity() {
 @PreviewScreenSizes
 @Composable
 fun VibrationEditorApp() {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
     NavigationSuiteScaffold(
         navigationSuiteItems = {
-            AppDestinations.entries.forEach {
+            AppDestinations.entries.forEach { destination ->
                 item(
                     icon = {
                         Icon(
-                            painterResource(it.icon),
-                            contentDescription = it.label
+                            painterResource(destination.icon),
+                            contentDescription = destination.label
                         )
                     },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
+                    label = { Text(destination.label) },
+                    selected = currentDestination?.hierarchy?.any { it.route == destination.route } == true,
+                    onClick = {
+                        navController.navigate(destination.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
                 )
             }
         }
     ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+        NavHost(
+            navController = navController,
+            startDestination = AppDestinations.PATTERNS.route,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            composable(AppDestinations.STUDIO.route) {
+                Studio()
+            }
+            composable(AppDestinations.PATTERNS.route) {
+                Patterns()
+            }
+            composable(AppDestinations.APPLICATIONS.route) {
+                Applications()
+            }
         }
     }
 }
@@ -68,10 +92,51 @@ fun VibrationEditorApp() {
 enum class AppDestinations(
     val label: String,
     val icon: Int,
+    val route: String
 ) {
-    HOME("Home", R.drawable.ic_home),
-    FAVORITES("Favorites", R.drawable.ic_favorite),
-    PROFILE("Profile", R.drawable.ic_account_box),
+    STUDIO("Studio", R.drawable.ic_home, "studio"),
+    PATTERNS("Patterns", R.drawable.ic_favorite, "patterns"),
+    APPLICATIONS("Applications", R.drawable.ic_account_box, "applications"),
+}
+
+@Composable
+fun Studio() {
+    Scaffold(
+        topBar = { Text("Studio") }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text("Studio Content")
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                val canvasQuadrantSize = size / 2F
+                drawRect(
+                    color = Color.Magenta,
+                    size = canvasQuadrantSize
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Patterns() {
+    Scaffold(
+        topBar = { Text("Patterns") }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text("Patterns Page")
+        }
+    }
+}
+
+@Composable
+fun Applications() {
+    Scaffold(
+        topBar = { Text("Applications") }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text("Applications Page")
+        }
+    }
 }
 
 @Composable
